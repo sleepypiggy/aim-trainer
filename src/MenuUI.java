3468b82ee73c84dd184461da5d9eb012e7044cbc
Randy <piggy01@students.cs.ubc.ca>
2026-01-19 23:41:19 -0800

    init commit

--- src/MenuUI.java
+++ src/MenuUI.java
@@ -5,10 +5,38 @@ import java.awt.event.ActionListener;
 
 public class MenuUI extends JPanel {
     public MenuUI(GameWindow gameWindow) {
+        // create buttons
         JButton startButton = new JButton("Start");
         JButton exitButton = new JButton("Exit");
-        add(startButton);
-        add(exitButton);
+
+        // create game title
+        JLabel gameTitle = new JLabel("Clicker Game");
+
+        // set Layout for this
+        setLayout(new BorderLayout());
+
+        // orient and add the buttons
+        JPanel buttons = new JPanel(new GridLayout(2, 1, 10, 10));
+        buttons.setOpaque(false);
+        buttons.add(startButton);
+        buttons.add(exitButton);
+
+        // container for the buttons for easy movement
+        JPanel buttonContainer = new JPanel(new GridBagLayout());
+
+        buttonContainer.add(buttons);
+
+        //container for game title for easy movement
+        JPanel titleContainer = new JPanel(new GridBagLayout());
+        titleContainer.setPreferredSize(new Dimension(400, 400));
+        titleContainer.add(gameTitle);
+
+        // add buttons to this, add constraint to place at bottom half
+        add(buttonContainer, BorderLayout.CENTER);
+
+        // add game title to this
+        add(titleContainer, BorderLayout.NORTH);
+
 
         startButton.addActionListener(new ActionListener() {
             @Override
